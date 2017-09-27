e48abc5453a508a6fbb4d77a6fecea462321b6c9
Stanley Ndagi <stanley@brighthive.io>
2017-09-27 08:32:51 -0500

    Add Dockerfiles

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
