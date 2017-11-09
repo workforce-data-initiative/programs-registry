e9e946b88ed564c919383dd3c64be727afbe53df
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-11-09 22:19:17 +0300

    [#152483748] refactor plan.sh by exporting gcc and libffi paths to library path

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,19 +1,28 @@
 pkg_origin=brighthive
 pkg_name=program-registry
-pkg_version=0.1.0
+pkg_version=0.0.1
 pkg_maintainer="jee@brighthive.io, stanley@brighthive.io, aretha@brighthive.io"
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_upstream_url="https://github.com/brighthive/program-registry.git"
 pkg_exports=([port]=listening_port)
 
+pkg_exposes=(port)
+pkg_build_deps=(
+    core/bash
+    core/coreutils
+    core/virtualenv
+    core/postgresql
+    core/gcc
+    core/libffi
+    core/openssl)
+pkg_deps=(core/python)
+pkg_interpreters=(bin/python3 bin/bash)
+
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
-
-pkg_exposes=(port)
-pkg_build_deps=(core/virtualenv core/postgresql core/gcc core/openssl)
-pkg_deps=(core/python)
-pkg_interpreters=(bin/python3)
+pkg_svc_user=root
+pkg_svc_group=root
 pkg_licence=('MIT')
 
 do_verify () {
@@ -25,17 +34,23 @@ do_clean() {
 }
 
 do_unpack() {
-  # create a env variable for the project root
   PROJECT_ROOT="${PLAN_CONTEXT}/.."
 
+  build_line "Copying project data from $PROJECT_ROOT to $pkg_prefix ..."
   mkdir -p $pkg_prefix
-  # copy the contents of the source directory to the habitat cache path
-  build_line "Copying project data from /program-registry to $pkg_prefix ..."
-  cp -r $PROJECT_ROOT/app $pkg_prefix/
-  cp -r $PROJECT_ROOT/instance $pkg_prefix/
-  cp -r $PROJECT_ROOT/tests $pkg_prefix/
-  cp -r $PROJECT_ROOT/*.py $pkg_prefix/
-  cp -r $PROJECT_ROOT/requirements.txt $pkg_prefix/
+  cp -vr $PROJECT_ROOT/app $pkg_prefix/
+  cp -vr $PROJECT_ROOT/instance $pkg_prefix/
+  cp -vr $PROJECT_ROOT/tests $pkg_prefix/
+  cp -vr $PROJECT_ROOT/*.py $pkg_prefix/
+  cp -vr $PROJECT_ROOT/requirements.txt $pkg_prefix/
+}
+
+do_prepare() {
+  export LD_LIBRARY_PATH="$(hab pkg path core/gcc)/lib"
+  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(hab pkg path core/libffi)/lib"
+  build_line "Updating pip and create virtual env..."
+  pip install --upgrade pip virtualenv
+  virtualenv "${pkg_prefix}" -p python3
 }
 
 do_build() {
@@ -43,12 +58,12 @@ do_build() {
 }
 
 do_install() {
-  cd $pkg_prefix
-  build_line "Creating virtual environment venv..."
-  virtualenv "venv" -p python3
-  . venv/bin/activate
+  build_line "Activating virtual env..."
+  source "${pkg_prefix}/bin/activate"
+
   build_line "Install gunicorn ..."
   pip install gunicorn
+  pip install --no-binary :all: $(grep psycopg2 requirements.txt)
   build_line "Installing requirements from requirements.txt ..."
   pip install -r requirements.txt
 }
