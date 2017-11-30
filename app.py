73d5c9ce83cf86baa8cb28a2793911c838b9247a
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-11-30 17:48:10 +0300

    [#152197760] refactor to pep8

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(flask_app)
 
- # import the blueprints and register it on the app
+# import the blueprints and register it on the app
 from app.organization import org_blueprint
 from app.programs import program_blueprint
 from app.services import service_blueprint
