1b8d83190de5bbc965d71b7bc1daa95018d912d6
Ndagi Stanley <stanley@brighthive.io>
2018-02-09 06:57:45 +0300

    Fix deploy fail

--- heroku.sh
+++ heroku.sh
@@ -1,4 +1,4 @@
-heroku container:login
+docker login --username=_ --password=$(heroku auth:token) registry.heroku.com
 if [ $CIRCLE_BRANCH = 'master' ]; then heroku git:remote -a programs-registry; fi
 if [ $CIRCLE_BRANCH = 'develop' ]; then heroku git:remote -a programs-registry-dev; fi
 heroku container:push web
