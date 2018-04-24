86ec9d24d0523a257665893b9449f487bf70a286
Aretha Kebirungi <aretha.kebirungi@andela.com>
2018-04-24 15:32:55 +0300

    [#155600023]push generic properties to parent Config

--- instance/config.py
+++ instance/config.py
@@ -8,21 +8,20 @@ class Config(object):
     DEBUG = True
     TESTING = False
     CSRF_ENABLED = True
-    SECRET = os.getenv('SECRET')
+    SECRET = os.environ.get('SECRET', 'Secret')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
 
 class TestingConfig(Config):
     """Configurations for Testing."""
     
     TESTING = True
-    SECRET = "Secret"
     DB_FILE_DESCRIPTOR, DATABASE = tempfile.mkstemp(suffix='.db')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATABASE)
 
@@ -32,6 +31,7 @@ class ProductionConfig(Config):
     
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 app_config = {
